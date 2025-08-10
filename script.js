3904f70aea787f6536a50696112147d74fa85bf3
nr0225 <74133613+nr0225@users.noreply.github.com>
2025-08-10 18:04:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+function toggleMenu(){const m=document.getElementById('menu'); m.style.display = (m.style.display==='block')?'none':'block';}
+document.getElementById('year') && (document.getElementById('year').textContent = new Date().getFullYear());
