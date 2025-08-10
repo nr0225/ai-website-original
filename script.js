c3fced8cf700779dbb0084967dfffe7715cbc390
nr0225 <74133613+nr0225@users.noreply.github.com>
2025-08-10 19:12:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-function toggleMenu(){const m=document.getElementById('menu'); m.style.display = (m.style.display==='block')?'none':'block';}
-document.getElementById('year') && (document.getElementById('year').textContent = new Date().getFullYear());
+function toggleMenu(){
+  const m=document.getElementById('menu');
+  m.style.display = (m.style.display==='block') ? 'none' : 'block';
+}
+const y=document.getElementById('year');
+if(y){ y.textContent=new Date().getFullYear(); }
